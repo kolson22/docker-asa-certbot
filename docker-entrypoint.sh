e2f2503e588e7e8df69e0f1ab96c0efde7406292
kolson22 <kyle.olson22@gmail.com>
2019-07-02 17:26:24 -0500

    first commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+acme.sh --issue --dns dns_he -d $domain > /dev/null
+
+openssl pkcs12 -export -in /root/.acme.sh/$domain/$domain.cer -inkey /root/.acme.sh/$domain/$domain.key -certfile /root/.acme.sh/$domain/ca.cer -out /tmp/acme.sh/$domain.p12 -passout pass:$key
+
+( echo -----BEGIN PKCS12-----; openssl base64 -in /tmp/acme.sh/$domain.p12; echo -----END PKCS12-----; )
